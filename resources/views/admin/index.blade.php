59b4ee8b3b568fe2eb9b2207a0eb80a72fc77dd7
Rahul-soni-01 <rahulsoni0000001@gmail.com>
2023-12-04 16:04:55 +0530

    admin - index 4 pm

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,71 +1,71 @@
 <x-layout.default>
-    <link rel="stylesheet" href="https://cdn.datatables.net/v/dt/jqc-1.12.4/dt-1.13.6/b-2.4.2/sl-1.7.0/datatables.min.css" />
+    <link rel="stylesheet"
+        href="https://cdn.datatables.net/v/dt/jqc-1.12.4/dt-1.13.6/b-2.4.2/sl-1.7.0/datatables.min.css" />
     <link rel="stylesheet" href="Editor-2.2.2/css/editor.dataTables.css">
-    
+
     <script src="https://cdn.datatables.net/v/dt/jqc-1.12.4/dt-1.13.6/b-2.4.2/sl-1.7.0/datatables.min.js"></script>
     <script src="Editor-2.2.2/js/dataTables.editor.js"></script>
-    
+
     <script>
-        
         $(document).ready(function() {
             $('#company-table').DataTable();
         });
     </script>
 
-@if(session('success'))
-<div class="alert alert-success">
-    {{ session('success') }}
-</div>
-@endif
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
 
-@if(session('error'))
-<div class="alert alert-danger">
-    {{ session('error') }}
-</div>
-@endif
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
 
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-<style>
-    .container_table{
-        width: 100%;
-        overflow-x: auto;
-        white-space: nowrap;
-    }
-    .alert-danger{
-        color: red;
-    }
-    .alert-success{
-        color: #5CB85C;
-    }
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <style>
+        .container_table {
+            width: 100%;
+            overflow-x: auto;
+            white-space: nowrap;
+        }
 
-    
+        .alert-danger {
+            color: red;
+        }
 
-</style>
+        .alert-success {
+            color: #5CB85C;
+        }
+    </style>
     <div class="container_table">
-        <a href="{{ route('admin.create') }}" class="btn btn-primary btn-sm mb-2" style="width: 50px; float:right;">Add</a>
-    <table id="company-table" class="table">
-        <thead>
-            <tr>
-                <th>Company Name 5656</th>
-                <th>Contact Person Full Name</th>
-                <th>Selected Plan</th>
-                <th>Plan Type</th>
-                <th>Start Date</th>
-                <th>End Date</th>
-                <th>Plan Amount</th>
-                <th>Paid Amount</th>
-                <th>Due Amount</th>
-                <th>Payment Status</th>
-                <th>Status (Running / Plan Over)</th>
-                {{-- <th>Website Address</th>
+        <a href="{{ route('admin.create') }}" class="btn btn-primary btn-sm mb-2"
+            style="width: 50px; float:right;">Add</a>
+        <table id="company-table" class="table">
+            <thead>
+                <tr>
+                    <th>Company Name </th>
+                    <th>Contact Person Full Name</th>
+                    <th>Selected Plan</th>
+                    <th>Plan Type</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>Plan Amount</th>
+                    <th>Paid Amount</th>
+                    <th>Due Amount</th>
+                    <th>Payment Status</th>
+                    <th>Status (Running / Plan Over)</th>
+                    {{-- <th>Website Address</th>
                 <th>Contact Number</th>
                 <th>Whatsapp Number</th>
                 <th>Email Id</th>
@@ -77,100 +77,143 @@
                 <th>Pin code</th>
                 <th>Currency</th>
                 <th>Currency Symbol</th> --}}
-                <th>Edit</th>
-                <th>Login Block Button</th>
-                <th>Direct login Button</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($companies as $company)
-            <tr>
-                <td>{{ $company->company_name }}</td>
-                <td>{{ $company->contact_person_full_name }}</td>
-                
-                    @foreach($datas as $data)
-                        @if($company->id == $data->company_name)
-                            @foreach($SubscriptionPlans as $SubscriptionPlan)
-                                @if($data->subscription_id == $SubscriptionPlan->id)
-                                <td>  {{ $SubscriptionPlan->plan_name }}</td>
-                                <td>  {{ $SubscriptionPlan->plan_type }}</td>
-                                <td>  {{ $data->start_date }}</td>
-                                <td>  {{ $data->end_date }}</td>
-                                {{-- <td>  {{ $SubscriptionPlan->plan_type }}</td> --}}
-                                @endif
-                            @endforeach
-                                
-                                <td> 
-                                @foreach($Transactions as $Transaction)
-                                @if($data->subscription_id == $Transaction->selected_plan)
-                                 {{ $Transaction->amount }} <br>
-                                @else 
-                               
-                                @endif
+                    <th>Edit</th>
+                    <th>Login Block Button</th>
+                    <th>Direct login Button</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($companies as $company)
+                    <tr>
+                        <td>{{ $company->company_name }}</td>
+                        <td>{{ $company->contact_person_full_name }}</td>
+
+                        @foreach ($datas as $data)
+                            @if ($company->id == $data->company_name)
+                                @foreach ($SubscriptionPlans as $SubscriptionPlan)
+                                    @if ($data->subscription_id == $SubscriptionPlan->id)
+                                        <td> {{ $SubscriptionPlan->plan_name }}</td>
+                                        <td> {{ $SubscriptionPlan->plan_type }}</td>
+                                        <td> {{ $data->start_date }}</td>
+                                        <td> {{ $data->end_date }}</td>
+                                        {{-- <td>  {{ $SubscriptionPlan->plan_type }}</td> --}}
+                                    @endif
                                 @endforeach
-                                </td>
-                                <td> 
-                                    @foreach($Transactions as $Transaction)
-                                    @if($data->subscription_id == $Transaction->selected_plan)
-                                     {{ $Transaction->collected_amount }} <br>
-                                    @else 
-                                   
-                                    @endif
+
+                                <td>
+                                    @php
+                                        $foundMatchingTransaction = false;
+                                    @endphp
+                                    @foreach ($Transactions as $Transaction)
+                                        @if ($data->subscription_id == $Transaction->selected_plan)
+                                            {{ $Transaction->amount }} <br>
+                                            @php
+                                                $foundTransaction = true;
+                                            @endphp
+                                        @endif
                                     @endforeach
-                                </td>
-                                <td> 
-                                    @foreach($Transactions as $Transaction)
-                                    @if($data->subscription_id == $Transaction->selected_plan)
-                                    <?php
-                                        $collected_amount = $Transaction->collected_amount;
-                                        $amount = $Transaction->amount;
-                                        $due = $collected_amount - $amount;
-                                        echo $due;
-                                    ?><br>
-                                    @else 
-                                   
+
+                                    @if (!$foundTransaction)
+                                        Not available
                                     @endif
-                                    @endforeach
-                                </td>
-                                <td> 
-                                    @foreach($Transactions as $Transaction)
-                                    @if($data->subscription_id == $Transaction->selected_plan)
-                                    <?php
-                                        $collected_amount = $Transaction->collected_amount;
-                                        $amount = $Transaction->amount;
-                                        $due = $collected_amount - $amount;
-                                        if ($due == '0'){
-                                            echo 'Paid';
-                                        }
-                                        else {
-                                            echo 'Unpaid';
-                                        }
-                                        
-                                    ?><br>
-                                    @else 
-                                   
-                                    @endif
-                                    @endforeach
                                 </td>
                                 <td>
-                                @foreach($SubscriptionPlans as $SubscriptionPlan)
-                                    @if($data->subscription_id == $SubscriptionPlan->id)
-                                    <?php
-                                    $end_date = $data->end_date;
+                                    @php
+                                        $foundTransaction = false;
+                                    @endphp
 
-                                    if (strtotime($end_date) < time()) {
-                                        echo 'Plan Over';
-                                    } else {
-                                        echo 'Running';
-                                    }
-                                    ?>  
+                                    @foreach ($Transactions as $Transaction)
+                                        @if ($data->subscription_id == $Transaction->selected_plan)
+                                            {{ $Transaction->collected_amount }}
+                                            @php
+                                                $foundTransaction = true;
+                                            @endphp
+                                            <br>
+                                        @endif
+                                    @endforeach
+
+                                    @if (!$foundTransaction)
+                                        Not available
                                     @endif
-                                @endforeach
                                 </td>
-                        @endif
-                    @endforeach
-                
-                {{-- <td>{{ $company->website_address }}</td>
+                                <td>
+                                    @php
+                                        $foundMatchingTransaction = false;
+                                    @endphp
+
+                                    @foreach ($Transactions as $Transaction)
+                                        @if ($data->subscription_id == $Transaction->selected_plan)
+                                            <?php
+                                            $collected_amount = $Transaction->collected_amount;
+                                            $amount = $Transaction->amount;
+                                            $due = $collected_amount - $amount;
+                                            echo $due;
+                                            ?><br>
+                                            @php
+                                                $foundMatchingTransaction = true;
+                                            @endphp
+                                        @endif
+                                    @endforeach
+
+                                    @if (!$foundMatchingTransaction)
+                                        Not available
+                                    @endif
+                                </td>
+                                <td>
+                                    @php
+                                        $foundMatchingTransaction = false;
+                                    @endphp
+                                    @foreach ($Transactions as $Transaction)
+                                        @if ($data->subscription_id == $Transaction->selected_plan)
+                                            <?php
+                                            $collected_amount = $Transaction->collected_amount;
+                                            $amount = $Transaction->amount;
+                                            $due = $collected_amount - $amount;
+                                            if ($due == '0') {
+                                                echo 'Paid';
+                                            } else {
+                                                echo 'Unpaid';
+                                            }
+                                            
+                                            ?><br>
+                                            @php
+                                                $foundTransaction = true;
+                                            @endphp
+                                        @else
+                                        @endif
+                                    @endforeach
+                                    @if (!$foundMatchingTransaction)
+                                        Not available
+                                    @endif
+                                </td>
+                                <td>
+                                    @php
+                                        $foundMatchingTransaction = false;
+                                    @endphp
+                                        @foreach ($SubscriptionPlans as $SubscriptionPlan)
+                                        @if ($data->subscription_id == $SubscriptionPlan->id)
+                                            <?php
+                                                $end_date = $data->end_date;
+                                                if (strtotime($end_date) < time()) {
+                                                    echo 'Plan Over';
+                                                } else {
+                                                    echo 'Running';
+                                                }
+                                            ?>
+                                            @php
+                                                $foundMatchingTransaction = true;
+                                            @endphp
+                                        @endif
+                                    @endforeach
+                                    
+                                    @if (!$foundMatchingTransaction)
+                                        Not available
+                                    @endif
+                                </td>
+                            @endif
+                        @endforeach
+
+                        {{-- <td>{{ $company->website_address }}</td>
                 <td>{{ $company->contact_number }}</td>
                 <td>{{ $company->whatsapp_number }}</td>
                 <td>{{ $company->email }}</td>
@@ -182,16 +225,17 @@
                 <td>{{ $company->pin_code }}</td>
                 <td>{{ $company->currency }}</td>
                 <td>{{ $company->currency_symbol }}</td> --}}
-               
-                <td>
-                    <a href="{{ route('admin.edit', ['id' => $company->id]) }}" class="btn btn-primary">Edit</a>
-                </td>
-                <td width="50px"><a class="btn btn-danger">Block</a></td> 
-                <td width="50px"><a class="btn btn-success">Login</a></td> 
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+
+                        <td>
+                            <a href="{{ route('admin.edit', ['id' => $company->id]) }}"
+                                class="btn btn-primary">Edit</a>
+                        </td>
+                        <td width="50px"><a class="btn btn-danger">Block</a></td>
+                        <td width="50px"><a class="btn btn-success">Login</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 
 </x-layout.default>
